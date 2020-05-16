128a7712c61c9ff33c3c2c924db1b1c038228eef
atomicgain <yyghdfz@126.com>
2020-05-16 09:55:10 +0000

    should_return_sellIn_n1_quality_50_when_sulfuras_pass_1_day_given_sellIn_n1_quality_50

--- src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/SulfurasTest.java
@@ -19,4 +19,11 @@ public class SulfurasTest {
         assertThat(sulfuras.getSellIn()).isEqualTo(-1);
         assertThat(sulfuras.getQuality()).isEqualTo(45);
     }
+    @Test
+    public void should_return_sellIn_n1_quality_50_when_sulfuras_pass_1_day_given_sellIn_n1_quality_50 () {
+        Sulfuras sulfuras = new Sulfuras(-1,50);
+        sulfuras.oneDayPass();
+        assertThat(sulfuras.getSellIn()).isEqualTo(-1);
+        assertThat(sulfuras.getQuality()).isEqualTo(50);
+    }
 }
