19d2b6c784d738c1ba84e3852cabce6ae454d056
atomicgain <yyghdfz@126.com>
2020-05-16 09:46:44 +0000

    should_return_sellIn_n2_quality_22_when_agedBrie_pass_1_day_given_sellIn_n1_quality_20

--- src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/AgedBrieTest.java
@@ -33,5 +33,12 @@ public class AgedBrieTest {
         assertThat(agedBrie.getSellIn()).isEqualTo(-1);
         assertThat(agedBrie.getQuality()).isEqualTo(22);
     }
+    @Test
+    public void should_return_sellIn_n2_quality_22_when_agedBrie_pass_1_day_given_sellIn_n1_quality_20 () {
+        AgedBrie agedBrie = new AgedBrie(-1,20);
+        agedBrie.oneDayPass();
+        assertThat(agedBrie.getSellIn()).isEqualTo(-2);
+        assertThat(agedBrie.getQuality()).isEqualTo(22);
+    }
 
 }
