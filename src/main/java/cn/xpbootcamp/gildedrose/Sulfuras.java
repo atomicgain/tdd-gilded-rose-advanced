334b54154b82d9bd25117ec1c3c33e4a098853a6
atomicgain <yyghdfz@126.com>
2020-05-16 09:53:46 +0000

    should_return_sellIn_0_quality_45_when_sulfuras_pass_1_day_given_sellIn_0_quality_45

---
+++ src/main/java/cn/xpbootcamp/gildedrose/Sulfuras.java
@@ -0,0 +1,24 @@
+package cn.xpbootcamp.gildedrose;
+
+public class Sulfuras {
+    private int sellIn;
+    private int quality;
+
+    public Sulfuras(int sellIn, int quality) {
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
+    public void oneDayPass() {
+        sellIn=sellIn;
+        quality=quality;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+}
